c5004a58486be689965ca8872e9afba05ac3b5da
IlnurNasybullin <ilnur.nasybullin2000@yandex.ru>
2022-05-09 09:45:20 +0300

    add namespace change file naming strategy

---
+++ cli/src/main/java/io/github/ilnurnasybullin/tagfm/cli/command/NamespaceChangeCommand.java
@@ -0,0 +1,33 @@
+package io.github.ilnurnasybullin.tagfm.cli.command;
+
+import io.github.ilnurnasybullin.tagfm.api.service.FileNamingStrategy;
+import io.github.ilnurnasybullin.tagfm.core.dto.namespace.NamespaceDto;
+import jakarta.inject.Singleton;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "change")
+@Singleton
+public class NamespaceChangeCommand implements Runnable {
+
+    private final FileManagerCommand fileManager;
+
+    @CommandLine.Option(names = {"-fns", "--file-naming-strategy"})
+    private FileNamingStrategy strategy;
+
+    public NamespaceChangeCommand(FileManagerCommand fileManager) {
+        this.fileManager = fileManager;
+    }
+
+    @Override
+    public void run() {
+        if (strategy != null) {
+            NamespaceDto namespace = fileManager.namespaceOrThrow();
+            if (namespace.fileNaming() != strategy) {
+                namespace = namespace
+                        .replaceFileNamingStrategy(strategy);
+                fileManager.setNamespace(namespace);
+                fileManager.setWriteMode();
+            }
+        }
+    }
+}
--- cli/src/main/java/io/github/ilnurnasybullin/tagfm/cli/command/NamespaceCommand.java
+++ cli/src/main/java/io/github/ilnurnasybullin/tagfm/cli/command/NamespaceCommand.java
@@ -4,7 +4,8 @@ import jakarta.inject.Singleton;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "namespace", subcommands = {
-        NamespaceRenameCommand.class
+        NamespaceRenameCommand.class,
+        NamespaceChangeCommand.class
 })
 @Singleton
 public class NamespaceCommand implements Runnable {
