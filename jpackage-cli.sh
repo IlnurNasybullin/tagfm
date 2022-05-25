c69d5dddbbb511337acb4fa736e1f622a495ad37
IlnurNasybullin <ilnur.nasybullin2000@yandex.ru>
2022-05-25 12:19:02 +0300

    change version

--- jpackage-cli.sh
+++ jpackage-cli.sh
@@ -3,7 +3,7 @@ jpackage --type exe \
   --name "tagfm" \
   --description "Tagged File Manager" \
   --vendor "Open Source" \
-  --app-version "0.0.15" \
+  --app-version "0.0.1" \
   --module-path "./cli/build/install/cli/lib;$JAVA_HOME/jmods" \
   --module "tagfm.cli/io.github.ilnurnasybullin.tagfm.cli.FileManagerCli" \
   --win-console \
