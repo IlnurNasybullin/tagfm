684f5b017be582dc93ff925fb9d4f996bb8aeb3e
IlnurNasybullin <ilnur.nasybullin2000@yandex.ru>
2022-06-05 00:05:51 +0300

    remove unnecessary package exporting

--- repository-xml/src/main/java/module-info.java
+++ repository-xml/src/main/java/module-info.java
@@ -30,5 +30,4 @@ module tagfm.repository.xml {
     provides NamespaceRepository with NamespaceRepositoryImpl;
 
     opens io.github.ilnurnasybullin.tagfm.repository.xml.entity to java.xml.bind;
-    exports io.github.ilnurnasybullin.tagfm.repository.xml.fileManager to java.xml.bind;
 }
