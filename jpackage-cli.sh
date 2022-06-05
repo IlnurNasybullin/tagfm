f9be71c286f31850568a0ab2630168d5449a2b41
IlnurNasybullin <ilnur.nasybullin2000@yandex.ru>
2022-06-05 17:17:15 +0300

    extracting modifiable variable

--- jpackage-cli.sh
+++ jpackage-cli.sh
@@ -1,10 +1,14 @@
 echo ">> Start jpackage"
+
+mainModuleName="tagfm.cli"
+mainClassName="io.github.ilnurnasybullin.tagfm.cli.TagFM"
+
 jpackage --type exe \
   --name "tagfm" \
   --description "Tagged File Manager" \
   --vendor "Open Source" \
   --app-version "0.0.1" \
   --module-path "./cli/build/install/cli/lib;$JAVA_HOME/jmods" \
-  --module "tagfm.cli/io.github.ilnurnasybullin.tagfm.cli.FileManagerCli" \
+  --module "$mainModuleName/$mainClassName" \
   --win-console \
   --win-dir-chooser
